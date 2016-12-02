1c9c8b80cc7334ee6ee2b5de0123e5a6b5d36673
E. R. C. Pereda <ercp42@gmail.com>
2016-12-02 12:04:48 -0300

    setup models for follower/followed relationship

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,   class_name:   "Relationship",
+                                    foreign_key:  "follower_id",
+                                    dependent:    :destroy
+  has_many :passive_relationships,  class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :following, throught: :active_relationships, source: :followed
+  has_many :followers, throught: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token
 
@@ -60,6 +68,21 @@ class User < ApplicationRecord
   # Defines a proto-feed.
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
